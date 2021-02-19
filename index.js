041de10a9c504814c772ba8e40b86d5c544cb8f9
QuincyZ <quincy.zinnemers@live.nl>
2021-02-19 14:27:55 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,25 @@
+const makePizza = () => {
+    console.log("Make dough")
+    console.log("Make tomato sauce")
+    console.log("Put the sauce on the dough")
+    console.log("Put cheese on the pizza")
+    console.log("Add toppings")
+    console.log("Bake the pizza")
+    console.log("Done")
+}
+
+const makeSushi = () => {
+    console.log("Prepare fish")
+    console.log("Make sushi rice")
+    console.log("Put rice on the seaweed")
+    console.log("Add your fish")
+    console.log("Roll up your sushi roll ")
+    console.log("Done")
+}
+
+makePizza()
+makePizza()
+
+makeSushi()
+makeSushi()
+makeSushi()
